c28c4bfabc9bf7b5a63ece7f559f32b738dd1f5f
Ace120C <145904614+Ace120C@users.noreply.github.com>
2025-10-07 17:00:00 +0100

    Add files via upload

--- scenes/char.c
+++ scenes/char.c
@@ -3,16 +3,29 @@
 
 SceneID CharSelectScene(void)
 {
+  Music CharSelect = LoadMusicStream("./music/char.mp3");
+  Texture2D NNProfile = LoadTexture("./assets/NN.png");
+  Texture2D GSProfile = LoadTexture("./assets/gold ship.png");
+  InitAudioDevice();
+  PlayMusicStream(CharSelect);
+
   SetTargetFPS(60);
+
   while (!WindowShouldClose()) {
+    UpdateMusicStream(CharSelect);
     BeginDrawing();
-    ClearBackground(BLACK);
-    DrawText("char select scene", 300, 300, 50, RAYWHITE);
+    ClearBackground(GRAY);
+    DrawTexture(NNProfile, 150, 50, RAYWHITE);
+    DrawTexture(GSProfile, 700, 50, RAYWHITE);
+    DrawText("Choose Your Character", 340, 670, 50, RED);
     EndDrawing();
 
     if (IsKeyPressed(KEY_T)) {
       return TITLE;
     }
   }
+  UnloadTexture(NNProfile);
+  UnloadTexture(GSProfile);
+  StopMusicStream(CharSelect);
   return CHAR_SELECT;
 }
