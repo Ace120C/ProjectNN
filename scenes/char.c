4852d083cf9027e3fec059103a5b8d3d5aa89d14
Ace120C <vegetotownley@proton.me>
2026-06-02 12:31:00 +0100

    adding more UI elements

--- scenes/char.c
+++ scenes/char.c
@@ -7,6 +7,7 @@ SceneID Skill_SelectScene(void)
   Texture2D Background = LoadTexture("./assets/bg.png");
   Texture2D Square = LoadTexture("./assets/square.png");
   Texture2D Square2 = LoadTexture("./assets/square2.png");
+  Texture2D Square3 = LoadTexture("./assets/square3.png");
   SceneID Next_Scene = SKILL_SELECT;
   PlayMusicStream(SkillMenu);
 
@@ -19,6 +20,7 @@ SceneID Skill_SelectScene(void)
     DrawTexture(Background, 0, 0, RAYWHITE);
     DrawTexture(Square, 10, 45, RAYWHITE);
     DrawTexture(Square2, 30, 370, RAYWHITE);
+    DrawTexture(Square3, 360, 20, RAYWHITE);
     EndDrawing();
 
     if (IsKeyPressed(KEY_T)) {
@@ -29,6 +31,7 @@ SceneID Skill_SelectScene(void)
   UnloadTexture(Background);
   UnloadTexture(Square);
   UnloadTexture(Square2);
+  UnloadTexture(Square3);
   StopMusicStream(SkillMenu);
   return Next_Scene;
 }
